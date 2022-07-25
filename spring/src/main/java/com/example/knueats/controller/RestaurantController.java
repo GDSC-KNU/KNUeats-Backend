bc25e60abb6435832d498b525c349608e8d65e7e
JangYunSeong <46878756+JangYunSeong@users.noreply.github.com>
2022-07-25 16:09:47 +0900

    refactory : url update

--- spring/src/main/java/com/example/knueats/controller/RestaurantController.java
+++ spring/src/main/java/com/example/knueats/controller/RestaurantController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/eats")
+@RequestMapping("")
 public class RestaurantController {
     @Autowired
     RestaurantRepository restaurantRepository;
